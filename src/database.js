e4e1156c8dede7bbc98e708b3b9a8b6f4a0ba272
Eduardo <eduar2.moreira@gmail.com>
2021-01-25 08:01:41 -0500

    ahora si

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const URI_ATLAS = process.env.MONGODB_ATLAS_URI;
 const URI = process.env.MONGODB_URI || 'mongodb://localhost/FyndPro';
 
 async function connect() {
-    await mongoose.connect(URI, {
+    await mongoose.connect(URI_ATLAS, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
